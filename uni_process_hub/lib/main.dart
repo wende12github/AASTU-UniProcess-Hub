abb442c0c0801be2f1f8f465cd2948205f890777
wende12github <wendmagegn9412@gmail.com>
2025-12-12 11:44:51 +0300

    firebase setup and integrate with flutter project

--- uni_process_hub/lib/main.dart
+++ uni_process_hub/lib/main.dart
@@ -1,51 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:uni_process_hub/screens/booking_screen.dart';
-import 'package:uni_process_hub/screens/forms_screen.dart';
-import 'package:uni_process_hub/screens/home_screen.dart';
-import 'package:uni_process_hub/screens/profile_screen.dart';
-import 'package:uni_process_hub/widgets/bottom_nav_bar.dart';
-import 'theme/app_theme.dart';
+import 'package:provider/provider.dart';
+import 'package:uni_process_hub/features/queue_status/widgets/ui/queue_status_screen.dart';
+import 'core/theme/app_theme.dart';
+import 'features/queue_status/controller/queue_status_controller.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const UinProcessApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'AASTU UniProcess Hub',
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      themeMode: ThemeMode.system,
-      home: MainScaffold(),
-    );
-  }
-}
-
-class MainScaffold extends StatefulWidget {
-  @override
-  _MainScaffoldState createState() => _MainScaffoldState();
-}
-
-class _MainScaffoldState extends State<MainScaffold> {
-  int _currentIndex = 0;
-
-  final List<Widget> _screens = [
-    HomeScreen(),
-    FormsScreen(),
-    BookingScreen(),
-    ProfileScreen(),
-  ];
+class UinProcessApp extends StatelessWidget {
+  const UinProcessApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_currentIndex],
-      bottomNavigationBar: AppBottomNavBar(
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AppTheme()),
+        ChangeNotifierProvider(create: (_) => QueueStatusController()),
+      ],
+      child: Consumer<AppTheme>(
+        builder: (context, theme, _) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'AASTU ASQS',
+            theme: theme.lightTheme,
+            darkTheme: theme.darkTheme,
+            themeMode: theme.themeMode,
+            home: const QueueStatusScreen(),
+          );
+        },
       ),
     );
   }
--- uni_process_hub/test/widget_test.dart
+++ uni_process_hub/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:uni_process_hub/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(UinProcessApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
